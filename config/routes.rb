83bd6ee480feba878ad01b4fdf07a1d70c273077
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-08-20 16:37:42 -0400

    Added Path to posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   resources :locations
   resources :users, only: [:index, :show]
 
-  post "https://journaling-app-backend.herokuapp.com//signin", to: "users#signin"
-  post "https://journaling-app-backend.herokuapp.com//signup", to: "users#signup"
-  get "https://journaling-app-backend.herokuapp.com//validate", to: "users#validate"
-  get "https://journaling-app-backend.herokuapp.com//user_posts", to: "users#user_posts"
-  get "https://journaling-app-backend.herokuapp.com//locations", to: "users#locations"
+  post "/signin", to: "users#signin"
+  post "/signup", to: "users#signup"
+  get "/validate", to: "users#validate"
+  get "/user_posts", to: "users#user_posts"
+  get "/locations", to: "users#locations"
 
 end
