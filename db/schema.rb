54498be161ff9a26787d24a89abbb6dd870cb23b
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-08-21 12:16:37 -0400

    Added location id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_091744) do
+ActiveRecord::Schema.define(version: 2019_08_21_161552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_091744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "tone"
+    t.integer "location_id"
   end
 
   create_table "users", force: :cascade do |t|
