d6761a902142f4e1d1db73f2317cff7960c01817
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-08-21 11:18:12 -0400

    Removed the problem migration (mood_id doens't exist)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_092008) do
+ActiveRecord::Schema.define(version: 2019_07_29_091744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2019_07_29_092008) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "location_id"
     t.string "tone"
   end
 
